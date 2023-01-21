62b39f40c1fcb50db7391e14142cc0168cf98640
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-01-21 16:41:56 +0530

    App Upgrade

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>E-Sence</h1>
-      <p>Build For Better Future</p>
-
-    </>
+    <div className="App">
+      <><div>
+        <h1>My First React App</h1>
+      </div>
+      </>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
