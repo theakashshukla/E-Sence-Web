44a2fc76b9da0886fff2b4135b848bf3da0a4711
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-01-25 16:21:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import SignupPage from './pages/Signup';
-import './App.css';
 import SigninPage from './pages/Signin';
+import './App.css';
+
 
 function App() {
 
   return (
-    <div className="App">
+    <>
       <SignupPage />
       <SigninPage />
-    </div>
+    </>
   );
 }
 export default App;
