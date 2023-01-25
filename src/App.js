ab9469f0e8df1721c9796836e8cf48a352a8b8a2
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-01-25 23:21:13 +0530

    revert changed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import './App.css';
 function App() {
 
   return (
-    <>
+    <div className='App'>
       <SignupPage />
       <SigninPage />
-    </>
+    </div>
   );
 }
 export default App;
