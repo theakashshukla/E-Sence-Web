9f410be2e516e9a5c1d8ae5e812830fee0b3a2c1
Devang Shahi <dvsgkp007@gmail.com>
2023-03-08 23:50:18 +0530

    sOME

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import SignupPage from './pages/Signup';
 import SigninPage from './pages/Signin';
 import './App.css';
+<<<<<<< Updated upstream
+=======
+import ForgotPassword from './pages/ForgetPassword';
+
+
+>>>>>>> Stashed changes
 
 
 function App() {
 
   return (
     <div className='App'>
+<<<<<<< Updated upstream
       <SignupPage />
       <SigninPage />
+=======
+      <ForgetPassword />
+>>>>>>> Stashed changes
     </div>
   );
 }
