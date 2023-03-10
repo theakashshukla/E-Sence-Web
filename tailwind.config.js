92cbadb2caf0e35bdcff89d1a708d084e0010255
Devang Shahi <dvsgkp007@gmail.com>
2023-03-10 23:51:58 +0530

    c

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,8 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const windmill = require("@windmill/react-ui/config");
-
-module.exports = windmill({
-  content: [
-    // Example content paths...
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx,vue}",
-  ],
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-      boxShadow: {
-        bottom:
-          "0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)",
-      },
-    },
+    extend: {},
   },
-});
+  plugins: [],
+};
