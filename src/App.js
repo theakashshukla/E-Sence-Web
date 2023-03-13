f943a361a371b2e849a6e9440b6285ba15264866
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-03-14 04:26:39 +0530

    App

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ export default function App() {
             <Route path="user" element={<UserProfile />} />
             <Route path="register" element={<RegisterPage />} />
           </Route>
-          
         </Routes>
       </Router>
     </div>
