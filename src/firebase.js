cd61949a7edd213060eb59706e18b130f7521a67
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-10-31 04:49:04 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -9,28 +9,28 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: `${process.env.FIREBASE_API_KEY}`,
-  authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-  storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${process.env.FIREBASE_MESSAGEING_SENDER_ID}`,
-  appId: `${process.env.FIREBASE_APP_ID}`,
-  measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
-console.log(`Firebase API Key: ${firebaseConfig.apiKey}`);
-console.log(`Firebase Auth Domain: ${firebaseConfig.authDomain}`);
+// const firebaseConfig = {
+//   apiKey: `${process.env.FIREBASE_API_KEY}`,
+//   authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+//   projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+//   storageBucket: `${process.env.FIREBASE_STORAGE_BUCKET}`,
+//   messagingSenderId: `${process.env.FIREBASE_MESSAGEING_SENDER_ID}`,
+//   appId: `${process.env.FIREBASE_APP_ID}`,
+//   measurementId: `${process.env.FIREBASE_MEASUREMENT_ID}`,
+// };
+
+// console.log(`Firebase API Key: ${firebaseConfig.apiKey}`);
+// console.log(`Firebase Auth Domain: ${firebaseConfig.authDomain}`);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
