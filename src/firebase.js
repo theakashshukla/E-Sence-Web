50578fd7600b6e05ae8643413a3f569aab09b3c6
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-10-31 05:04:17 +0530

    ch

--- src/firebase.js
+++ src/firebase.js
@@ -35,4 +35,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const auth = getAuth();
