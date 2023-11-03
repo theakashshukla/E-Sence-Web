9901f8879af3c56cea09abb214c5ce09a0f24661
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-11-03 19:52:28 +0530

    update

--- app/(dashboard)/dashboard/settings/page.tsx
+++ app/(dashboard)/dashboard/settings/page.tsx
@@ -1,34 +1,34 @@
-import { redirect } from "next/navigation"
+// import { redirect } from "next/navigation"
 
-// import { authOptions } from "@/lib/auth"
-// import { getCurrentUser } from "@/lib/session"
-// import { DashboardHeader } from "@/components/header"
-import { DashboardShell } from "@/components/shell"
-// import { UserNameForm } from "@/components/user-name-form"
+// // import { authOptions } from "@/lib/auth"
+// // import { getCurrentUser } from "@/lib/session"
+// // import { DashboardHeader } from "@/components/header"
+// import { DashboardShell } from "@/components/shell"
+// // import { UserNameForm } from "@/components/user-name-form"
 
-export const metadata = {
-  title: "Settings",
-  description: "Manage account and website settings.",
-}
+// export const metadata = {
+//   title: "Settings",
+//   description: "Manage account and website settings.",
+// }
 
-export default function SettingsPage() {
-//   const user = await getCurrentUser()
+// export default function SettingsPage() {
+// //   const user = await getCurrentUser()
 
-//   if (!user) {
-//     redirect(authOptions?.pages?.signIn || "/login")
-//   }
+// //   if (!user) {
+// //     redirect(authOptions?.pages?.signIn || "/login")
+// //   }
 
-  return (
-    <DashboardShell>
-     <div><h1>helloo</h1></div>
-       {/* <DashboardHeader
-        heading="Settings"
-        text="Manage account and website settings."
-      >
-      <div className="grid gap-10">
-        <UserNameForm user={{ id: user.id, name: user.name || "" }} />
-      </div> */}
-    </DashboardShell>
+//   return (
+//     <DashboardShell>
+//      <div><h1>helloo</h1></div>
+//        {/* <DashboardHeader
+//         heading="Settings"
+//         text="Manage account and website settings."
+//       >
+//       <div className="grid gap-10">
+//         <UserNameForm user={{ id: user.id, name: user.name || "" }} />
+//       </div> */}
+//     </DashboardShell>
     
-  )
-}
+//   )
+// }
--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -1,12 +1,12 @@
-// import Pricing from "@/components/layout/pricing/pricing";
-// import PricingFeature from "@/components/layout/pricing/pricing-features";
+import Pricing from "@/components/layout/pricing/pricing";
+import PricingFeature from "@/components/layout/pricing/pricing-features";
 
 export default function PricingPage(){
     return(
         <>
         <h1>Hello</h1>
-        {/* <Pricing/>
-        <PricingFeature/> */}
+        <Pricing/>
+        <PricingFeature/>
         </>
     )
 }
