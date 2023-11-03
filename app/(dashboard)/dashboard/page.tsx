1e71f45ec7314764e2624ef296992ce424b7bd95
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-11-03 19:58:51 +0530

    update

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,25 +1,25 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
-// // import { authOptions } from "@/lib/auth"
-// // import { db } from "@/lib/db"
-// // import { getCurrentUser } from "@/lib/session"
-// // import { EmptyPlaceholder } from "@/components/empty-placeholder"
-// import { DashboardHeader } from "@/components/header"
-// // import { PostCreateButton } from "@/components/post-create-button"
-// import { DashboardShell } from "@/components/shell"
+// import { authOptions } from "@/lib/auth"
+// import { db } from "@/lib/db"
+// import { getCurrentUser } from "@/lib/session"
+// import { EmptyPlaceholder } from "@/components/empty-placeholder"
+import { DashboardHeader } from "@/components/header"
+// import { PostCreateButton } from "@/components/post-create-button"
+import { DashboardShell } from "@/components/shell"
 
-// export const metadata = {
-//   title: "Dashboard",
-// };
+export const metadata = {
+  title: "Dashboard",
+};
 
-// export default async function DashboardPage() {
-//   return (
-//     <DashboardShell>
-//       <DashboardHeader heading="Posts" text="Create and manage posts.">
-//         {/* <PostCreateButton /> */}
-//       </DashboardHeader>
+export default async function DashboardPage() {
+  return (
+    <DashboardShell>
+      <DashboardHeader heading="Posts" text="Create and manage posts.">
+        {/* <PostCreateButton /> */}
+      </DashboardHeader>
 
-//     </DashboardShell>
+    </DashboardShell>
     
-//   );
-// }
+  );
+}
