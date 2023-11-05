da7e1338fea35361c5c0142d90959e8d25adab04
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-11-05 23:15:03 +0530

    Add empty div to Banner component

--- components/layout/banner.tsx
+++ components/layout/banner.tsx
@@ -6,6 +6,7 @@ export default function Banner() {
         className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
         aria-hidden="true"
       >
+        
         <div
           className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
           style={{
