db81605211b704ed98f17f22ffa0c7b8143dd4d7
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2023-12-31 04:46:37 +0530

    Remove unnecessary h1 tag in PricingPage component

--- app/(marketing)/pricing/page.tsx
+++ app/(marketing)/pricing/page.tsx
@@ -4,7 +4,7 @@ import PricingFeature from "@/components/layout/pricing/pricing-features";
 export default function PricingPage(){
     return(
         <>
-        <h1>Hello</h1>
+
         <Pricing/>
         <PricingFeature/>
         </>
